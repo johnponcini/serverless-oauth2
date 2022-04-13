0acce66e09edcc84ace5a5f3c9fb2dfaffbb2163
John Poncini <john@maps.org>
2022-04-13 13:53:59 -0700

    removed current user method

--- app/main/views.py
+++ app/main/views.py
@@ -9,12 +9,6 @@ from app.oauth import require_oauth
 
 main = Blueprint("main", __name__)
 
-def current_user():
-    if "id" in session:
-        uid = session["id"]
-        return User.query.get(uid)
-    return None
-
 
 def split_by_crlf(s):
     return [v for v in s.splitlines() if v]
