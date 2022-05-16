9e98d4772b9131100ed5c71d40dd56e6d05c7f92
John Poncini <john@maps.org>
2022-05-16 13:08:27 -0600

    chenged stripe api version

--- app/main/views.py
+++ app/main/views.py
@@ -13,7 +13,7 @@ from app.oauth.forms import RegisterClientForm
 import stripe
 
 stripe.api_key =  os.environ.get("STRIPE_SECRET_KEY")
-stripe.api_version = "2020-08-07"
+stripe.api_version = "2020-08-27"
 
 main = Blueprint("main", __name__)
 
