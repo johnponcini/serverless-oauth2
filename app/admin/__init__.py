7d47f17d4c91e6450b0a1f45e1f90495b7f8c650
John Poncini <john@maps.org>
2022-05-18 12:23:52 -0600

    added admin role based accesibility

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,11 +1,27 @@
 from flask_admin import Admin
+from flask_login import current_user
 from flask_admin.contrib.sqla import ModelView
 
 from app.models.user import User, Role
 from app.models.oauth2 import OAuth2Client
 
+class AdminView(ModelView):
+
+    can_export = True
+
+    def is_accessible(self):
+        if not current_user.is_active or not current_user:
+            return False
+        if current_user.role_id == 1:
+            self.can_create = True
+            self.can_edit = True
+            self.can_delete = True
+            self.can_export = True
+            return True
+        return False
+
 def config_admin(app, db):
     admin = Admin(app)
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Role, db.session))
-    admin.add_view(ModelView(OAuth2Client, db.session))
+    admin.add_view(AdminView(User, db.session))
+    admin.add_view(AdminView(Role, db.session))
+    admin.add_view(AdminView(OAuth2Client, db.session))
