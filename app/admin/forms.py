5354eb6532b87d6281ec880ece4fe31653bcd70a
John Poncini <john@maps.org>
2022-05-18 13:24:55 -0600

    added salesforce functions

---
+++ app/admin/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import ValidationError
+from wtforms.fields import (
+    BooleanField,
+    PasswordField,
+    StringField,
+    SubmitField,
+    RadioField,
+    EmailField,
+    IntegerField
+)
+from wtforms.validators import Email, EqualTo, InputRequired, Length
+
+class CreateContactForm(FlaskForm):
+    first_name = StringField("First Name", validators=[InputRequired(), Length(1, 64)])
+    last_name = StringField("Last Name", validators=[InputRequired(), Length(1, 64)])
+    email = EmailField("Email", validators=[InputRequired(), Length(1, 64), Email()])
+    submit = SubmitField("Create Contact")
+
+class CreateDonationForm(FlaskForm):
+    email = EmailField("Email", validators=[InputRequired(), Length(1, 64), Email()])
+    amount = IntegerField("Amount", validators=[InputRequired])
+    submit = SubmitField("Donate")
---
+++ app/admin/views.py
@@ -0,0 +1,29 @@
+import os
+import time
+import stripe
+from simple_salesforce import Salesforce
+
+from flask import Blueprint, request, session, url_for
+from flask import render_template, redirect, jsonify
+from flask_login import current_user, login_required
+
+from app.admin.forms import CreateContactForm, CreateDonationForm
+
+sf = Salesforce(instance_url="https://maps501c3--arkustest.lightning.force.com", username="digital@maps.org", password="eu3HxxbyX!J_", security_token="dUdkHC149SfzJkdC0YVXYf0W", domain="test")
+
+admin = Blueprint("admin", __name__)
+
+
+@app.route("/create-customer", methods=["GET", "POST"])
+def create_customer():
+    form = CreateContactForm
+
+    sf.Contact.create(
+        {
+            'FirstName' : form.first_name.data, 
+            'LastName': form.last_name.data, 
+            'Email': form.email.data,
+        }
+    )
+
+    return redirect(url_for('admin'))
