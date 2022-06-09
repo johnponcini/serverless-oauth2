8b8254ef1659404e49db2675a33f97cf479f2e04
John Poncini <john@maps.org>
2022-06-09 17:21:17 -0500

    updated db credentials

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = create_app(
         "SECRET_KEY": "secret",
         "OAUTH2_REFRESH_TOKEN_GENERATOR": True,
         "SQLALCHEMY_TRACK_MODIFICATIONS": False,
-        "SQLALCHEMY_DATABASE_URI": "mysql+pymysql://admin:password@oauth-db.cswiy5vmwmww.us-west-2.rds.amazonaws.com",
+        "SQLALCHEMY_DATABASE_URI": "mysql+pymysql://admin:ubQMnSvN6LHc4nQ!@oauth2.cswiy5vmwmww.us-west-2.rds.amazonaws.com",
     }
 )
