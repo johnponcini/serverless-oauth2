a39906100d33133916eff1a19d6803cc21bc9589
John Poncini <john@maps.org>
2022-06-10 07:43:10 -0700

    changed unique requirement for user names

--- app/models/user.py
+++ app/models/user.py
@@ -19,7 +19,7 @@ class User(UserMixin, db.Model):
     __tablename__ = "user"
     id = db.Column("id", db.Integer, primary_key=True)
     confirmed = db.Column(db.Boolean, default=False)
-    name = db.Column(db.String(64), unique=True, index=True)
+    name = db.Column(db.String(64), index=True)
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey("role.id"))
