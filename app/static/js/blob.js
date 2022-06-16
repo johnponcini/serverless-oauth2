38c56a1e71b2393ca0f27f0189964d4c76b1ce6c
John Poncini <john@maps.org>
2022-06-16 15:17:55 -0700

    reduced time variable

--- app/static/js/blob.js
+++ app/static/js/blob.js
@@ -3,7 +3,7 @@ function randy(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
-var time = 2000,
+var time = 2,
     tl = new TimelineMax({repeat: -1,yoyo:false}),
     container = document.getElementById("container");
 for (var i = 0; i < blobCount; i++) {
