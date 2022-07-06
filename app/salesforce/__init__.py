f4fcddd39d1d319c79e6975024a283a179f9b776
John Poncini <john@maps.org>
2022-07-05 20:05:34 -0700

    removed recurring donation campaign

--- app/salesforce/__init__.py
+++ app/salesforce/__init__.py
@@ -115,7 +115,6 @@ class Recurring_Donation:
                 'npe03__Amount__c': amount,
                 'Name': 'API Test',
                 'npe03__Installment_Period__c': recurring,
-                'npe03__Recurring_Donation_Campaign': '7012f000000bOFfAAM',
                 'npsp__CardExpirationMonth__c': exp_month,
                 'npsp__CardExpirationYear__c': exp_year,
                 'npsp__CardLast4__c': card_last_4
